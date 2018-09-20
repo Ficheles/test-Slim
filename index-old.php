48244d7b5a577a38f6a5389a33cf46c4eb577f74
Unimed Goiania/TI - Rafael Fideles <rafael.fideles@unimedgoiania.coop.br>
2018-09-20 18:04:22 -0300

    adicionado curl para teste de ticket

--- index-old.php
+++ index-old.php
@@ -15,3 +15,8 @@
     return $response;
   });
   $app->run();
+
+
+curl -X POST http://localhost:8000/v1/ticket -H "Content-type: application/json" \
+     -H "X-Token:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiQGZpZGVsaXNzYXVybyIsInR3aXR0ZXIiOiJodHRwczpcL1wvdHdpdHRlci5jb21cL2ZpZGVsaXNzYXVybyIsImdpdGh1YiI6Imh0dHBzOlwvXC9naXRodWIuY29tXC9tbXNmaWRlbGlzIn0.XUQnRUb5E7_BA7XXo7-5y_08KxuS3apX7U4Jnkmp3Go" \
+     -d '{"code":"1112", "old":"", "program":"SGU2.0", "program_bio":"CAD443", "department":"Cadastro", "subject":"MIF046 - Ajustar emissão de etiquetas do beneficiário " }' 
